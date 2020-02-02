cabcec8a71e5fd73c6caba063cdb72e2867d3feb
enteraether <thetheresagarcia@gmail.com>
2020-02-01 21:57:07 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Coupon.create(coupon_code: '123', store: 'local')
+Coupon.create(coupon_code: '456', store: 'non local')
